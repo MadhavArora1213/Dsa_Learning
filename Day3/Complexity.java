dec35d2d98aa0cfe0690e02df8847653c9abe6d1
MadhavArora1213 <madhavarora132005@gmail.com>
2025-06-17 22:08:49 +0530

    Learn how the time and space complexity work

---
+++ Day3/Complexity.java
@@ -0,0 +1,14 @@
+
+public class Complexity {
+
+    public static void main(String[] args) {
+        
+        // Time Complexity of this is O(n^2)
+        
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         System.out.println("i: " + i + ", j: " + j);
+        //     }
+        // }
+    }
+}
