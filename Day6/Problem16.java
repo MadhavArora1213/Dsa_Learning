062021f5fd56846ae58b1ea3c7df4b6c2f681318
MadhavArora1213 <madhavarora132005@gmail.com>
2025-06-21 20:11:28 +0530

    Do Pattern16

--- Day6/Problem16.java
+++ Day6/Problem16.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
 public class Problem16 {
     public static void main(String[] args) {
-        int n;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n: ");
-        n = sc.nextInt();
-
+     
         // 
-        for (int i = 0; i < n; i++) {
-            for (int j =0; j <i; j++) {
-                 System.out.print((char)((int)('A'+i)) + " ");
+      int rows = 5; // You can change this value for more rows
+
+        for (int i = 1; i <= rows; i++) {
+            char ch = (char) ('A' + i - 1); // Calculate the character
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
             }
-            System.out.println();
+            System.out.println(); // Move to the next line
         }
     }
 }
