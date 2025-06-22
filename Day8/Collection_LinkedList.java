f26037a24f7a3cacd9efe0f1a91bf177ded660bd
MadhavArora1213 <madhavarora132005@gmail.com>
2025-06-22 10:45:08 +0530

    Learn LinkedList in Collection Framework

---
+++ Day8/Collection_LinkedList.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedList;
+
+public class Collection_LinkedList {
+    public static void main(String[] args) {
+        
+        LinkedList<String> cars = new LinkedList<>();
+
+        // Adding elements to the LinkedList
+        cars.add("BMW");
+        cars.add("Volvo");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        System.out.println(cars);
+    }
+}
