4ffa47eea93bb254530535bc881f28bf0b86eb80
MadhavArora1213 <madhavarora132005@gmail.com>
2025-06-23 23:02:48 +0530

    DO List sorting in collection

--- Day9/List_Sorting.java
+++ Day9/List_Sorting.java
@@ -1,7 +1,19 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class List_Sorting {
     public static void main(String[] args) {
         
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Cherry");
+        list.add("Date");
+
+        Collections.sort(list);
+        System.out.println("Sorted List: " + list);
+
+        // Sorting in reverse order
+        Collections.sort(list, Collections.reverseOrder());
     }
 }
